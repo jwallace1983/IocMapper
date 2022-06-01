b14a09cb40761a7a077e1383f2b71677b4b8be5c
Jason Wallace <wallace.jason@gmail.com>
2022-05-31 23:52:55 -0400

    Add reflector to track binaries

---
+++ IocMapper.UnitTests/ReflectorTest.cs
@@ -0,0 +1,29 @@
+﻿namespace IocMapper.UnitTests
+{
+    public class ReflectorTest
+    {
+        [Fact]
+        public void AssembliesTest()
+        {
+            // Arrange
+            // Act
+            var results = Reflector.Assemblies;
+
+            // Assert
+            results.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void AddAssemblyTest()
+        {
+            // Arrange
+            // Act
+            Reflector.AddAssembly(typeof(ReflectorTest));
+            Reflector.AddAssembly(typeof(Mocks.LifetimeService));
+            Reflector.AddAssembly(typeof(Mocks.IDefaultService));
+
+            // Assert
+            Reflector.Assemblies.Should().HaveCount(1);
+        }
+    }
+}
---
+++ IocMapper/Reflector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace IocMapper
+{
+    public static class Reflector
+    {
+        public static IEnumerable<Assembly> Assemblies => _assemblies;
+        private static readonly List<Assembly> _assemblies = new List<Assembly>();
+
+        public static void AddAssembly(Type type)
+        {
+            if (_assemblies.Any(a => a.FullName.Equals(type.Assembly.FullName)))
+                return; // Guard: already added
+            _assemblies.Add(type.Assembly);
+        }
+    }
+}
